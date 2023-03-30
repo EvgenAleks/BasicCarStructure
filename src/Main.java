5fcd4e3255a1f79d56a29cf547d2352c7e8eefc5
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-03-30 21:56:22 +0200

    the engine specs have been added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         audi.color = "black";
         audi.model = "A3";
         audi.power = "210";
+
+        Engine x = new Engine();
+        x.cylinders="4";
+        x.size="3300";
+        x.weight="420";
     }
 }
